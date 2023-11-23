b262cc24717b4c03defb647842e6db6e8d4c650f
MarcosFvb <marcos_venanco@hotmail.com>
2023-11-23 04:50:05 -0300

    Movimentação concluída
    Movimentação concluída em partes, agora é possível andar para os dois
    lados e lançar a estrela.

--- Assets/player_movement.cs
+++ Assets/player_movement.cs
@@ -1,53 +1,126 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Adjust the speed as needed
-    public float climbSpeed = 3f; // Speed for climbing
 
-    private Rigidbody2D rb;
-    private bool isClimbing = false;
+    public float moveSpeed = 5f;
+    public bool isClimbing = false;
+    public bool isThrowing = false;
+    public Rigidbody2D rb;
+    public Animator animator;
+    private Vector2 movement;
+    public GameObject ninjaStarPrefab; // Prefab of the ninja star
+    public float minPower = 0.5f; // Minimum power for throwing
+    public float maxPower = 2f; // Maximum power for throwing
+    public float powerIncreaseSpeed = 1.5f; // Speed of the power meter increase
+    public Gradient powerGradient; // Gradient for the power meter
+    public bool isFacingRight = true;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    private bool isCharging = false;
+    private float currentPower = 0f;
+    private bool powerIncreasing = true;
 
     void Update()
     {
-        float horizontalMove = Input.GetAxisRaw("Horizontal");
-        float verticalMove = Input.GetAxisRaw("Vertical");
 
-        Vector2 movement = new Vector2(horizontalMove, verticalMove);
+        if(isClimbing) {
+            movement.y = Input.GetAxisRaw("Vertical");
+            animator.SetInteger("verticalMovement", (int)movement.y);
+            handleFacing();
+        } else {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            animator.SetInteger("horizontalMovement", (int)movement.x);
+            handleFacing();
+        }
+        animator.SetFloat("speed", movement.sqrMagnitude);
 
-        if (!isClimbing)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
+            isCharging = true;
+            animator.SetBool("isThrowing", true);
+            animator.Play("ThrowAnimation", -1, 0); // Start the animation at the first frame
+            animator.speed = 0f; // Pause the animation
+        }
+
+        if (isCharging)
+        {
+            HandlePowerMeter();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isCharging = false;
+            powerIncreasing = true;
+            animator.SetBool("isThrowing", false);
+            animator.speed = 1f;
+
+            // Calculate the power based on the position of the power meter
+            currentPower = Mathf.Lerp(minPower, maxPower, currentPower);
+
+            // Throw the star with the calculated power
+            ThrowStar(currentPower);
+            currentPower = 0f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+    void HandlePowerMeter()
+    {
+        Debug.Log(currentPower);
+        if (powerIncreasing)
+        {
+            currentPower += powerIncreaseSpeed * Time.deltaTime;
+
+            if (currentPower >= maxPower)
+            {
+                currentPower = maxPower;
+                powerIncreasing = false;
+            }
         }
         else
         {
-            rb.velocity = new Vector2(rb.velocity.x, movement.y * climbSpeed);
+            currentPower -= powerIncreaseSpeed * Time.deltaTime;
+
+            if (currentPower <= minPower)
+            {
+                currentPower = minPower;
+                powerIncreasing = true;
+            }
+        }
+
+    }
+
+    void ThrowStar(float power)
+    {
+        Vector2 throwPosition = rb.transform.position; // Initial throw position (can be adjusted)
+
+        // Calculate the direction to throw the star based on player's facing direction
+        Vector2 throwDirection = transform.right * (transform.localScale.x > 0 ? 1 : -1);
+
+        // Offset the throw position to be in front of the ninja
+        throwPosition += throwDirection * 0.5f; // Adjust the offset distance as needed
+
+        GameObject star = Instantiate(ninjaStarPrefab, throwPosition, Quaternion.identity);
+        Rigidbody2D starRb = star.GetComponent<Rigidbody2D>();
+
+        // Apply force to the star based on the calculated power and direction
+        starRb.velocity = throwDirection * power;
+    }
+
+    void handleFacing() {
+        if(movement.x < 0 && isFacingRight) {
+            flip();
+            isFacingRight = false;
+        } else if(movement.x > 0 && !isFacingRight) {
+            flip();
+            isFacingRight = true;
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        // Check collision with walls to switch between walking and climbing
-        if (collision.gameObject.CompareTag("Wall"))
-        {
-            isClimbing = true;
-        }
-    }
-
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        // Exit climbing mode when not in contact with the wall
-        if (collision.gameObject.CompareTag("Wall"))
-        {
-            isClimbing = false;
-        }
+    void flip() {
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
-
